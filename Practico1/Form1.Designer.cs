2446f923422c701d45b7ad91c91a905065d96ae7
daniiii1222 <danielarecalde999@gmail.com>
2026-08-19 18:49:03 -0300

    Agregar archivos de proyecto.

---
+++ Practico1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Practico1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BGuardar = new Button();
+            BEliminar = new Button();
+            LApellido = new Label();
+            LNombre = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // BGuardar
+            // 
+            BGuardar.Location = new Point(32, 156);
+            BGuardar.Name = "BGuardar";
+            BGuardar.Size = new Size(94, 29);
+            BGuardar.TabIndex = 0;
+            BGuardar.Text = "Guardar";
+            BGuardar.UseVisualStyleBackColor = true;
+            BGuardar.Click += button1_Click;
+            // 
+            // BEliminar
+            // 
+            BEliminar.Location = new Point(151, 156);
+            BEliminar.Name = "BEliminar";
+            BEliminar.Size = new Size(94, 29);
+            BEliminar.TabIndex = 1;
+            BEliminar.Text = "Eliminar";
+            BEliminar.UseVisualStyleBackColor = true;
+            BEliminar.Click += button2_Click;
+            // 
+            // LApellido
+            // 
+            LApellido.AutoSize = true;
+            LApellido.Location = new Point(32, 57);
+            LApellido.Name = "LApellido";
+            LApellido.Size = new Size(66, 20);
+            LApellido.TabIndex = 2;
+            LApellido.Text = "Apellido";
+            LApellido.Click += label1_Click;
+            // 
+            // LNombre
+            // 
+            LNombre.AutoSize = true;
+            LNombre.Location = new Point(32, 102);
+            LNombre.Name = "LNombre";
+            LNombre.Size = new Size(64, 20);
+            LNombre.TabIndex = 3;
+            LNombre.Text = "Nombre";
+            LNombre.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(120, 57);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 34);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(120, 102);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(270, 54);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(200, 154);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(376, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 7;
+            button1.Text = "Salir";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(482, 253);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(LNombre);
+            Controls.Add(LApellido);
+            Controls.Add(BEliminar);
+            Controls.Add(BGuardar);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Mi primer forms";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BGuardar;
+        private Button BEliminar;
+        private Label LApellido;
+        private Label LNombre;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
---
+++ Practico1/Form1.cs
@@ -0,0 +1,35 @@
+namespace Practico1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox1.Text + " " + textBox2.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
